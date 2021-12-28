9f90be6de3ebf74f34a5b84803b6697303916398
Jac Wynn <jacquinwynn91@gmail.com>
2021-12-27 20:22:43 -0500

    install storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,27 @@
+import '../src/styles/globals.scss';
+
+//============================ https://dev.to/jonasmerlin/how-to-use-the-next-js-image-component-in-storybook-1415
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => (
+    <OriginalNextImage
+      {...props}
+      unoptimized
+    />
+  ),
+});
+//============================
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
